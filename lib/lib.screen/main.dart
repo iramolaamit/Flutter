87049bc32f66f3963f87845d2e43226401a333e4
AMIT CHAND RAMOLA <aramola20@gmail.com>
2022-08-07 00:37:43 +0530

    day3

---
+++ lib/lib.screen/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/lib.screen/loginPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        // home: HomePage(),
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.light,
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+        ),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.red,
+        ),
+        initialRoute: "/home",
+        routes: {
+          "/": (context) => LoginPage(),
+          // home screen or default route
+          "/home": (context) => LoginPage(),
+          "/Login": (context) => LoginPage(),
+        });
+  }
+}
